124f35dc168fc4a699fe43977d054c0800293684
Elvis Sedic <esedic@gmail.com>
2013-09-15 21:59:57 +0200

    Inital commit

---
+++ assets/devicejs.js
@@ -0,0 +1,157 @@
+(function() {
+  var _addClass, _doc_element, _find, _handleOrientation, _hasClass, _orientation_event, _removeClass, _supports_orientation, _user_agent,
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+ 
+  window.device = {};
+ 
+  _doc_element = window.document.documentElement;
+ 
+  _user_agent = window.navigator.userAgent.toLowerCase();
+ 
+  device.ios = function() {
+    return device.iphone() || device.ipod() || device.ipad();
+  };
+ 
+  device.iphone = function() {
+    return _find('iphone');
+  };
+ 
+  device.ipod = function() {
+    return _find('ipod');
+  };
+ 
+  device.ipad = function() {
+    return _find('ipad');
+  };
+ 
+  device.android = function() {
+    return _find('android');
+  };
+ 
+  device.androidPhone = function() {
+    return device.android() && _find('mobile');
+  };
+ 
+  device.androidTablet = function() {
+    return device.android() && !_find('mobile');
+  };
+ 
+  device.blackberry = function() {
+    return _find('blackberry') || _find('bb10') || _find('rim');
+  };
+ 
+  device.blackberryPhone = function() {
+    return device.blackberry() && !_find('tablet');
+  };
+ 
+  device.blackberryTablet = function() {
+    return device.blackberry() && _find('tablet');
+  };
+ 
+  device.windows = function() {
+    return _find('windows');
+  };
+ 
+  device.windowsPhone = function() {
+    return device.windows() && _find('phone');
+  };
+ 
+  device.windowsTablet = function() {
+    return device.windows() && _find('touch');
+  };
+ 
+  device.mobile = function() {
+    return device.androidPhone() || device.iphone() || device.ipod() || device.windowsPhone() || device.blackberryPhone();
+  };
+ 
+  device.tablet = function() {
+    return device.ipad() || device.androidTablet() || device.blackberryTablet() || device.windowsTablet();
+  };
+ 
+  device.portrait = function() {
+    return Math.abs(window.orientation) !== 90;
+  };
+ 
+  device.landscape = function() {
+    return Math.abs(window.orientation) === 90;
+  };
+ 
+  _find = function(needle) {
+    return _user_agent.indexOf(needle) !== -1;
+  };
+ 
+  _hasClass = function(class_name) {
+    var regex;
+    regex = new RegExp(class_name, 'i');
+    return _doc_element.className.match(regex);
+  };
+ 
+  _addClass = function(class_name) {
+    if (!_hasClass(class_name)) {
+      return _doc_element.className += " " + class_name;
+    }
+  };
+ 
+  _removeClass = function(class_name) {
+    if (_hasClass(class_name)) {
+      return _doc_element.className = _doc_element.className.replace(class_name, "");
+    }
+  };
+ 
+  if (device.ios()) {
+    if (device.ipad()) {
+      _addClass("ios ipad tablet");
+    } else if (device.iphone()) {
+      _addClass("ios iphone mobile");
+    } else if (device.ipod()) {
+      _addClass("ios ipod mobile");
+    }
+  } else if (device.android()) {
+    if (device.androidTablet()) {
+      _addClass("android tablet");
+    } else {
+      _addClass("android mobile");
+    }
+  } else if (device.blackberry()) {
+    if (device.blackberryTablet()) {
+      _addClass("blackberry tablet");
+    } else {
+      _addClass("blackberry mobile");
+    }
+  } else if (device.windows()) {
+    if (device.windowsTablet()) {
+      _addClass("windows tablet");
+    } else if (device.windowsPhone()) {
+      _addClass("windows mobile");
+    } else {
+      _addClass("desktop");
+    }
+  } else {
+    _addClass("desktop");
+  }
+ 
+  _handleOrientation = function() {
+    if (device.landscape()) {
+      _removeClass("portrait");
+      return _addClass("landscape");
+    } else {
+      _removeClass("landscape");
+      return _addClass("portrait");
+    }
+  };
+ 
+  _supports_orientation = __indexOf.call(window, "onorientationchange") >= 0;
+ 
+  _orientation_event = _supports_orientation ? "orientationchange" : "resize";
+ 
+  if (window.addEventListener) {
+    window.addEventListener(_orientation_event, _handleOrientation, false);
+  } else if (window.attachEvent) {
+    window.attachEvent(_orientation_event, _handleOrientation);
+  } else {
+    window[_orientation_event] = _handleOrientation;
+  }
+ 
+  _handleOrientation();
+ 
+}).call(this);
---
+++ devicedetector.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * ---------------------------------------------------------------------------------------------------------
+ * Device Detect plugin
+ *
+ * Version 1.0.0
+ *
+ * Copyright (C) 2013 Spletodrom. All rights reserved.
+ *
+ * Device Detect detector is free software and is distributed under the GNU General Public License,
+ * and as distributed it may include or be derivative of works licensed under the GNU
+ * General Public License or other free or open source software licenses.
+ * ---------------------------------------------------------------------------------------------------------
+ **/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.plugin.plugin' );
+define ('PLG_URL', JURI::root(true).'/plugins/system/plg_devicedetector');
+
+/**
+ * UADetector Plugin
+ */
+class plgSystemDevicedetect extends JPlugin
+{
+	public function onAfterInitialise()
+	{
+		$doc = JFactory::getDocument();
+		if ($this->params->get('jsversion', 1)) {
+			$doc->addScript(PLG_URL . '/assets/devicejs.min.js');
+		}
+		else {
+			$doc->addScript(PLG_URL . '/assets/devicejs.js');
+		}
+	}
+}
